bfb3d6e769bb628776771e883019765145fcead6
Tomas <tomas@getkevin.io>
2019-10-18 16:24:34 +0300

    Tx sending added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const Client = require('bitcoin-core');
 const config = require('./config.js');
 
@@ -12,7 +17,7 @@ const client = new Client({
 });
 
 function handleError(err, res) {
-  if (err.code === 'ECONNREFUSED') {
+  if (err && err.code === 'ECONNREFUSED') {
     console.log(err);
     res.status(400).json({ message: 'Bitcoin node is not running.' });
   } else {
@@ -21,15 +26,9 @@ function handleError(err, res) {
   }
 }
 
-function getNewAddress() {
-  return client.getNewAddress();
-}
-function getTransaction(txId) {
-  return client.getTransaction(txId);
-}
 app.get('/', async (req, res) => {
   try {
-    const address = await getNewAddress();
+    const address = await client.getNewAddress();
     res.json({ address });
   } catch (err) {
     handleError(err, res);
@@ -39,7 +38,7 @@ app.get('/', async (req, res) => {
 app.get('/tx', (req, res) => {
   const txId = req.query.txid;
   let txData = {};
-  getTransaction(txId).then((receivedTxData) => {
+  client.getTransaction(txId).then((receivedTxData) => {
     txData = receivedTxData;
     console.log(`TxId: ${txData.txid}`);
     if (txData.confirmations > 0) {
@@ -53,5 +52,52 @@ app.get('/tx', (req, res) => {
   });
 });
 
-app.listen(3000);
-console.log('Listening on port:', 3000);
+function collectInputs(inputsArray, target) {
+  return new Promise((resolve, reject) => {
+    const result = [];
+    const formatedInputs = [];
+    let sum = 0;
+    for (let i = 0; i < inputsArray.length; i += 1) {
+      if (sum < target) {
+        result.push(inputsArray[i]);
+        formatedInputs.push({ txid: inputsArray[i].txid, vout: inputsArray[i].vout });
+        sum += inputsArray[i].amount;
+      }
+    }
+    if (sum >= target) {
+      resolve({ result, formatedInputs, sum: sum.toFixed(8) });
+    } else {
+      reject({ message: 'Not enouth funds.' });
+    }
+  });
+}
+
+app.post('/send', async (req, res) => {
+  const { fromAddress, amount, toAddress } = req.body;
+  try {
+    const unspentTxs = await client.listUnspent(0, 999999999, [fromAddress]);
+    const inputsToSend = await collectInputs(unspentTxs, amount + config.fixedFee);
+    const outputsArray = [];
+    const destination = {};
+    destination[toAddress] = (amount).toFixed(8);
+    outputsArray.push(destination);
+    if (amount + config.fixedFee < Number(inputsToSend.sum)) {
+      const change = {};
+      change[fromAddress] = (Number(inputsToSend.sum) - amount - config.fixedFee).toFixed(8);
+      outputsArray.push(change);
+    }
+    const rawTx = await client.createRawTransaction(inputsToSend.formatedInputs, outputsArray);
+    const signedTx = await client.signRawTransactionWithWallet(rawTx);
+    if (signedTx.hex) {
+      const txId = await client.sendRawTransaction(signedTx.hex);
+      res.json({ txId });
+    } else {
+      handleError({ message: 'Something went wrong.' }, res);
+    }
+  } catch (err) {
+    handleError(err, res);
+  }
+});
+
+app.listen(config.port);
+console.log('Listening on port:', config.port);
--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const config = {
+  port: 3010,
   wallet: {
     host: 'localhost',
     port: 8332,
     user: 'local',
     pass: 'local',
   },
+  fixedFee: 0.0001,
 };
 
 module.exports = config;
